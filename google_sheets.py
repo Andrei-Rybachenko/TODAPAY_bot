73f23e3fafb6f248b3baed36c6e5a468bdb6fce5
Andrei Rybachenko <andreirybachenko1999@gmail.com>
2025-07-24 13:09:49 +0300

    Updating google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -1,19 +1,34 @@
 import gspread
+import os
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 
+
+GOOGLE_CREDENTIALS = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(GOOGLE_CREDENTIALS, scope)
+client = gspread.authorize(credentials)
+
 SHEET_NAME = "TODA_PAY_Users"
-CRED_FILE = "credentials.json"  # Путь к JSON-файлу ключа от сервисного аккаунта
-
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name(CRED_FILE, scope)
-client = gspread.authorize(creds)
-sheet = client.open(SHEET_NAME).sheet1  # Первый лист в таблице
 
 
-def save_user_to_sheet(username, user_id, time):
-    sheet.append_row([username, str(user_id), time])
+def save_resume_to_sheet(username, user_id, email, summary, timestamp):
+    sheet = client.open(SHEET_NAME).worksheet("Resumes")
+    sheet.append_row([username, str(user_id), email, summary, timestamp])
 
 
-def save_resume_to_sheet(username, user_id, email, summary, time):
-    resume_sheet = client.open(SHEET_NAME).worksheet("Resumes")
-    resume_sheet.append_row([username, str(user_id), email, summary, time])
+def save_user_to_sheet(username, user_id, timestamp):
+    sheet = client.open(SHEET_NAME).sheet1
+    sheet.append_row([username, str(user_id), timestamp])
+
+
+def export_all_content():
+    sheet = client.open(SHEET_NAME).worksheet("Resumes")
+    return sheet.get_all_values()
